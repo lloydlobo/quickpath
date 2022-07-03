9a12c8611e83d77455c8a1744d528119bb61d1b4
lloydlobo <lloyd@lloydlobo.com>
2022-07-03 14:04:33 +0530

    feat: initialize component and node & add to App

---
+++ src/Quickpath/QuickpathVisualizer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function QuickpathVisualizer() {
+  return (
+    <div className="QuickpathVisualizer">
+      <p>Hello, world!</p>
+    </div>
+  );
+}
+
+export default QuickpathVisualizer;
